04e427e23b9c4690319cd4f236cf2004b0ba853b
milman20 <thegooners303@gmail.com>
2023-07-15 14:25:13 +0700

    projek dialer motor

--- application/helpers/pustaka_helper.php
+++ application/helpers/pustaka_helper.php
@@ -6,7 +6,7 @@ function cek_login()
 
     if (!$ci->session->userdata('email')) {
         $ci->session->set_flashdata('pesan', '<div class="alert alert-danger" role="alert">Akses ditolak. Anda belum login!! </div>');
-        redirect('autentifikasi');
+        redirect('/autentifikasi');
     } else {
         $role_id = $ci->session->userdata('role_id');
     }
